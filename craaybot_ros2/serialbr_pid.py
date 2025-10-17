fa6be79abd043a8c5e79a7c17d3ff5e9abe2bae0
Eric <eriico39hi@gmail.com>
2025-10-17 19:07:37 -0400

    PID tuning
    Made PID tuning vals into ROS parameters. Started to tune, but I think i killed my 12V supply........

--- craaybot_ros2/serialbr_pid.py
+++ craaybot_ros2/serialbr_pid.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import JointState
+from rcl_interfaces.msg import SetParametersResult
 import serial
 import threading
 import math
@@ -11,12 +12,9 @@ import math
 CTS_PER_REV = 984
 RADS_PER_CT = (2 * math.pi) / CTS_PER_REV
 
-KP = 15.0
-KI = 5.0
-KD = 0.1
 MAX_INT = 100
 
-PORT_NAME = '/dev/ttyACM1'
+PORT_NAME = '/dev/ttyACM0'
 
 #PID tuning class
 class PIDController:
@@ -70,15 +68,29 @@ class SerialBridge(Node):
         #Serial setup - opens serial port @ 115200 baud
         self.ser = serial.Serial(PORT_NAME, 115200, timeout=1)
         self.get_logger().info("Connected to Arduino!")
-        self.get_logger().info(f"PID Gains: Kp = {KP}, Ki = {KI}, Kd = {KD}")
+
+        
+        #PID tuning vals as ROS parameters
+        self.declare_parameter('kp',20)
+        self.declare_parameter('ki',0.1)
+        self.declare_parameter('kd',5.0)
+        
+        kp = self.get_parameter('kp').value
+        ki = self.get_parameter('ki').value
+        kd = self.get_parameter('kd').value
+        
+        self.get_logger().info(f"PID Gains: Kp = {kp}, Ki = {ki}, Kd = {kd}")
 
         #ROS setup - pubs encoder data, subscribes to "drive_command" and does callback upon receiving one
         self.pub = self.create_publisher(JointState, 'joint_states', 10)
         self.sub = self.create_subscription(Float32MultiArray, 'speed', self.speed_callback, 10)
         
         #PID controllers for each wheel (same gains for now)
-        self.leftpid = PIDController(KP,KI,KD,MAX_INT)
-        self.rightpid = PIDController(KP,KI,KD,MAX_INT)
+        self.leftpid = PIDController(kp,ki,kd,MAX_INT)
+        self.rightpid = PIDController(kp,ki,kd,MAX_INT)
+        
+        #Allow live parameter updates
+        self.add_on_set_parameters_callback(self.on_param_change)
         
         #init speeds
         self.leftsetspeed = 0.0
@@ -95,6 +107,26 @@ class SerialBridge(Node):
         #start constant serial read thread
         self.running = True
         threading.Thread(target=self.read_serial, daemon=True).start()
+
+    def on_param_change(self, params):
+        
+        for param in params:
+            if param.name == 'kp':
+                self.leftpid.kp = param.value
+                self.rightpid.kp = param.value
+            elif param.name == 'ki':
+                self.leftpid.ki = param.value
+                self.rightpid.ki = param.value
+            elif param.name == 'kd':
+                self.leftpid.kd = param.value
+                self.rightpid.kd = param.value
+                
+        kp = self.leftpid.kp
+        ki = self.leftpid.ki
+        kd = self.leftpid.kd
+        self.get_logger().info(f"PID Gains: Kp = {kp}, Ki = {ki}, Kd = {kd}")
+
+        return SetParametersResult(successful = True)
 
     #function for sending data to arduino
     #right now just f and s (forward and stop)
